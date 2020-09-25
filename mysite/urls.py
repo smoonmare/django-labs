6e0b527886a55f1c79249d96ab8b806561b5ebd0
smoonmare <smoonmare@gmail.com>
2020-09-25 04:44:30 +0000

    Added working home page. Added hello application for cookie/session practice

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,14 +3,18 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import url
 from django.views.static import serve
+from django.views.generic import TemplateView
 
 # Up two folders to serve "site" content
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SITE_ROOT = os.path.join(BASE_DIR, 'site')
 
+app_name='mysite'
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='home/main.html')),
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),                                                                                           
+    path('polls/', include('polls.urls')),
+    path('hello/', include('hello.urls')),
     url(r'^site/(?P<path>.*)$', serve,
         {'document_root': SITE_ROOT, 'show_indexes': True},
         name='site_path'

b8ab7fe37888a00ef5f393a69c811c048e8552b9
smoonmare <smoonmare@gmail.com>
2020-10-02 20:51:25 +0000

    Added fully finished 'Ads' app. settings.py & urls.py as well as /home are modified from previous stage for C3W5 lab. All of original files are stores in backup folder for future substitution.

--- cats/admin.py
+++ cats/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from cats.models import Breed, Cat
+from ads.models import Breed, Cat 
 
 # Register your models here.
-admin.site.register(Breed)
 admin.site.register(Cat)
+admin.site.register(Breed)

913853a5183fa0b071d89a0c7492b3199a3aebf7
smoonmare <smoonmare@gmail.com>
2020-10-05 23:06:01 +0000

    Added CSV for UNESCO App. Modified many_load.py to work with UNESCO data model. Added context of CSV to DB

--- scripts/many_load.py
+++ scripts/many_load.py
@@ -1,34 +1,38 @@
 import csv  # https://docs.python.org/3/library/csv.html
-
-# https://django-extensions.readthedocs.io/en/latest/runscript.html
-
-# python3 manage.py runscript many_load
-
-from unesco.models import 
+from unesco.models import Site, Category, States, Region, Iso
 
 
 def run():
-    fhand = open('many/load.csv')
+    fhand = open('unesco/whc-sites-2018-clean.csv')
     reader = csv.reader(fhand)
     next(reader)  # Advance past the header
 
-    Person.objects.all().delete()
-    Course.objects.all().delete()
-    Membership.objects.all().delete()
-
+    Site.objects.all().delete()
+    Category.objects.all().delete()
+    States.objects.all().delete()
+    Region.objects.all().delete()
+    Iso.objects.all().delete()
     # Format
-    # email,role,course
-    # jane@tsugi.org,I,Python
-    # ed@tsugi.org,L,Python
-
+    # name_0,description_1,justification_2,
+    # year_3,longitude_4,latitude_5,
+    # area_hectares_6,category_7,states_8,region_9,iso_10
     for row in reader:
-        print(row)
-
-        p, created = Person.objects.get_or_create(email=row[0])
-        c, created = Course.objects.get_or_create(title=row[2])
-
-        r = Membership.LEARNER
-        if row[1] == 'I':
-            r = Membership.INSTRUCTOR
-        m = Membership(role=r, person=p, course=c)
-        m.save()
+        # print(row)
+        try:
+            y = int(row[3])
+            lg = float(row[4])
+            lt = float(row[5])
+            ah = float(row[6])
+        except:
+            y = None
+            lg = None
+            lt = None
+            ah = None
+        c, created = Category.objects.get_or_create(name=row[7])
+        s, created = States.objects.get_or_create(name=row[8])
+        r, created = Region.objects.get_or_create(name=row[9])
+        i, created = Iso.objects.get_or_create(name=row[10])
+        s = Site(name=row[0], description=row[1], justification=row[2],
+                year=y, longitude=lg, latitude=lt, 
+                area_hectares=ah, category=c, states=s, region=r, iso=i)
+        s.save()
